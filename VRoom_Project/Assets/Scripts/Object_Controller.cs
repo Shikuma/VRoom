82f4170e4f71123fb1f8b1c9ae736880a209a69d
VRRobbie <rmorrissey@unomaha.edu>
2016-02-06 17:42:24 -0600

    Simple Raycast Control
    Raycast and select() method are set up with no functionality. Added then
    removed possible solution for UI with VR. Not solved.

---
+++ VRoom_Project/Assets/Scripts/Object_Controller.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Object_Controller : MonoBehaviour {
+
+    public bool selectable = false;
+
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void Select()
+    {
+        if(selectable)
+        {
+            Debug.Log("This object is selectable");
+            //highlight object
+            //display object name
+            //hit space to play audio, boolean in update
+        }
+    }
+}
---
+++ VRoom_Project/Assets/Scripts/Player_Controller.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player_Controller : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Vector3 fwd = transform.TransformDirection(Vector3.forward);
+        Ray ray = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
+        RaycastHit hit;
+        if(Physics.Raycast(ray, out hit, 10))
+        {
+            print("Collided with" + hit.collider.gameObject.name);
+            Debug.DrawLine(ray.origin, hit.point);
+            if(hit.collider.gameObject.GetComponent<Object_Controller>() != null)
+            {
+                hit.collider.gameObject.GetComponent<Object_Controller>().Select();
+            }
+        }
+	}
+}
