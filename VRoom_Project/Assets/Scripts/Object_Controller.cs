ed2c39ef51c8560d1c9e02427f40841c84820946
asdgsd <sgdf>
2016-02-07 22:04:33 -0600

    -In Progress- Object highlighting
    Highlighting objects is partially done. Issue with highlighting multiple
    poly's of a single objects. (Like the bed has 5 children, the highlight
    shows up funny) I attempted joining multiple objects into one, but it
    just didn't work.

--- VRoom_Project/Assets/Scripts/Object_Controller.cs
+++ VRoom_Project/Assets/Scripts/Object_Controller.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 
 public class Object_Controller : MonoBehaviour {
-
+	public Shader highlightShader;
     public bool selectable = false;
 
 	void Start () {
@@ -14,14 +14,29 @@ public class Object_Controller : MonoBehaviour {
 	
 	}
 
-    public void Select()
-    {
-        if(selectable)
-        {
+    public void Select(){
+        if(selectable){
             Debug.Log("This object is selectable");
             //highlight object
+			if (GetComponent<Renderer> () != null) {
+				GetComponent<Renderer> ().material.shader = highlightShader;
+				print ("Setting shader for parent");
+			} else
+				print ("Parent renderer is null");
+			if(transform.childCount > 0){
+				print (gameObject.name);
+				foreach(Transform t in transform){
+					print (t.name);
+
+					t.GetComponent<Renderer>().material.shader = highlightShader;
+				}
+			}
+		
+			//GetComponent<Renderer>().material.shader = highlightShader;
             //display object name
             //hit space to play audio, boolean in update
         }
     }
+
+
 }
--- VRoom_Project/Assets/Scripts/Player_Controller.cs
+++ VRoom_Project/Assets/Scripts/Player_Controller.cs
@@ -2,25 +2,24 @@
 using System.Collections;
 
 public class Player_Controller : MonoBehaviour {
-
+	private bool shaderSet = true;
 	// Use this for initialization
 	void Start () {
 	
 	}
 	
 	// Update is called once per frame
-	void Update ()
-    {
+	void Update (){
         Vector3 fwd = transform.TransformDirection(Vector3.forward);
         Ray ray = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
         RaycastHit hit;
-        if(Physics.Raycast(ray, out hit, 10))
-        {
-            print("Collided with" + hit.collider.gameObject.name);
+        if(Physics.Raycast(ray, out hit, 10)){
+            //print("Collided with " + hit.collider.gameObject.name);
             Debug.DrawLine(ray.origin, hit.point);
-            if(hit.collider.gameObject.GetComponent<Object_Controller>() != null)
-            {
+            if(hit.collider.gameObject.GetComponent<Object_Controller>() != null && shaderSet){
+				print ("Calling select() for: " + hit.collider.gameObject.name);
                 hit.collider.gameObject.GetComponent<Object_Controller>().Select();
+				shaderSet = false;
             }
         }
 	}
