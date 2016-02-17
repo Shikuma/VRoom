2f9bad4e9de79ccd79698d383b2e00a8112a7f6b
asdgsd <sgdf>
2016-02-16 22:41:15 -0600

    Lots of changes look in description
    -Object selection and deselection is done and attached to, what I think,
    is ever object we need. As well as group selection.
    -Temp materials applied to most object
    - UI working properly with VR (hopefully)
    -Object name display when selected
    -Audio trigger areas (currently do nothing)
    -Attached mesh colliders on nearly everything
    -Disabled doors for the time being

--- VRoom_Project/Assets/Scripts/Object_Controller.cs
+++ VRoom_Project/Assets/Scripts/Object_Controller.cs
@@ -1,10 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+using UnityEngine.UI;
 public class Object_Controller : MonoBehaviour {
 	public Shader highlightShader;
     public bool selectable = false;
-
+	public GameObject[] objectGroup;
+	public string objName = "Object Name";
+	public Text name;
 	void Start () {
 	
 	}
@@ -20,23 +22,27 @@ public class Object_Controller : MonoBehaviour {
             //highlight object
 			if (GetComponent<Renderer> () != null) {
 				GetComponent<Renderer> ().material.shader = highlightShader;
-				print ("Setting shader for parent");
-			} else
-				print ("Parent renderer is null");
-			if(transform.childCount > 0){
-				print (gameObject.name);
-				foreach(Transform t in transform){
-					print (t.name);
-
-					t.GetComponent<Renderer>().material.shader = highlightShader;
+				for (int i = 0; i < objectGroup.Length; i++) {
+					objectGroup [i].GetComponent<Renderer> ().material.shader = highlightShader;
 				}
-			}
-		
+
+			} else Debug.Log ("Parent renderer is null");
+
+			name.text = objName;
+
 			//GetComponent<Renderer>().material.shader = highlightShader;
             //display object name
             //hit space to play audio, boolean in update
         }
     }
+
+	public void Deselect(){
+		GetComponent<Renderer> ().material.shader = Shader.Find ("Standard");
+		for (int i = 0; i < objectGroup.Length; i++) {
+			objectGroup [i].GetComponent<Renderer> ().material.shader = Shader.Find ("Standard");
+		}
+		name.text = "";
+	}
 
 
 }
--- VRoom_Project/Assets/Scripts/Player_Controller.cs
+++ VRoom_Project/Assets/Scripts/Player_Controller.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class Player_Controller : MonoBehaviour {
 	private bool shaderSet = true;
+	private GameObject currObj = null;
 	// Use this for initialization
 	void Start () {
 	
@@ -14,11 +15,17 @@ public class Player_Controller : MonoBehaviour {
         Ray ray = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
         RaycastHit hit;
         if(Physics.Raycast(ray, out hit, 10)){
+			GameObject hitObj = hit.collider.gameObject;
             //print("Collided with " + hit.collider.gameObject.name);
             Debug.DrawLine(ray.origin, hit.point);
-            if(hit.collider.gameObject.GetComponent<Object_Controller>() != null){
+			if(hitObj.GetComponent<Object_Controller>() != null){
 				print ("Calling select() for: " + hit.collider.gameObject.name);
-                hit.collider.gameObject.GetComponent<Object_Controller>().Select();
+				//print("Callind Deselect() for: " + 
+				if (currObj != null)
+					currObj.GetComponent<Object_Controller> ().Deselect ();
+				hitObj.GetComponent<Object_Controller> ().Select();
+				currObj = hitObj;
+				
             }
         }
 
